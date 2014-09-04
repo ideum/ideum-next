51a857c6c2af59bfb532f90ee7626b5b31e8b971
Ben Cates <ben@ideum.com>
2014-09-04 09:10:13 -0600

    Add reveal animation to footer

--- _assets/javascripts/scripts.js
+++ _assets/javascripts/scripts.js
@@ -1,13 +1,16 @@
 //= require jquery
 //= require angular/angular
+//= require angular/angular-animate
 //= require ./slider
 //= require_self
+//= require ./animations
 //= require ./footer
 
 (function () {
   'use strict';
 
   var App = angular.module('ideum', [
+    'ngAnimate',
     'ideum.slider'
   ]);
 
