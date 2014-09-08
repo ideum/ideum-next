c5977f69d891f59ed8fbbaaebdcd411714906426
Ben Cates <ben@ideum.com>
2014-09-08 17:28:04 -0600

    Set up select2 and converted homepage selects

--- _assets/javascripts/scripts.js
+++ _assets/javascripts/scripts.js
@@ -1,9 +1,14 @@
 //= require jquery
+//= require select2
 //= require angular/angular
 //= require angular/angular-animate
 //= require angular/angular-touch
+//
 //= require angular-carousel
+//= require angular-ui-select2
+//
 //= require_self
+//
 //= require ./animations
 //= require ./header
 //= require ./footer
@@ -16,7 +21,8 @@
     'ngAnimate',
 
     // Third-party libraries
-    'angular-carousel'
+    'angular-carousel',
+    'ui.select2'
 
     // Project code
   ]);
